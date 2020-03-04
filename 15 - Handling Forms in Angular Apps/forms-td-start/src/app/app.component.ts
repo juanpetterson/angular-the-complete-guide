9235ee6455cae49bbc6697c74d615ef428173c2d
juanpetterson <juan.petterson@gmail.com>
2020-03-03 22:08:10 -0300

    Imlemented a little project using the Template-Driven Forms approach

---
+++ 15 - Handling Forms in Angular Apps/forms-td-start/src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f', { static: true }) signupForm: NgForm;
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submited: boolean = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value);
+  // } // used with peoperty binding
+
+  onSubmit() {
+    this.submited = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
+}
